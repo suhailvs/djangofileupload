b574efc6a9c2e9410507f0c541901dfab85f17c5
suhailvs <suhailvs@gmail.com>
2018-07-09 07:58:59 +0530

    added postgre database

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,6 +124,10 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+#if 'DATABASE_URL' in os.environ:
+import dj_database_url
+DATABASES = {'default': dj_database_url.config()}
+
 ALLOWED_HOSTS = ['*']
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
